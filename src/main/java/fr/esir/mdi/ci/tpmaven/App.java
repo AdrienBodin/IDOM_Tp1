0cd0029ba13e3b8e0f14f306a29d15acc7a56c10
AdrienBodin <adrien.bodin@etudiant.univ-rennes1.fr>
2021-01-29 11:22:30 +0100

    first commit

---
+++ src/main/java/fr/esir/mdi/ci/tpmaven/App.java
@@ -0,0 +1,22 @@
+package fr.esir.mdi.ci.tpmaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    
+    
+    /**
+     * Il y avait deja de la javadoc mais j'en remets quand même 
+     */
+    
+    /*
+     * je test avec des commentaires pour voir aussi
+     */
+}
